ca9b823f4b5dd08db695a453a52701c577d4139d
krishna100204 <krishnasolankey@gmail.com>
2024-12-29 22:52:48 +0530

    schema.js

--- schema.js
+++ schema.js
@@ -1,11 +1,21 @@
 const Joi = require('joi');
+const review = require('./models/review');
+
 module.exports.listingSchema = Joi.object({
-    listing : Joi.object({
+    listing: Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
-        location: Joi.string().required(),
+        location: Joi.string().allow("", null), 
         country: Joi.string().required(),
-        price: Joi.string().required().min(0),
-        location: Joi.string().allow("", null),
+        price: Joi.number().required().min(0), 
+        image: Joi.string() 
+    }).required()
+});
+
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        comment: Joi.string().required(),
     }).required(),
 });
