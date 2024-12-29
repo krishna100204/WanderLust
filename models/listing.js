21df73e642d56f03dd2830601b73f039a6be3278
krishna100204 <krishnasolankey@gmail.com>
2024-12-29 23:04:36 +0530

    listing.js

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
+
+
 
 const listingSchema = new Schema ({
     title: {
@@ -21,7 +24,11 @@ const listingSchema = new Schema ({
         }
     ]
 });
-
+listingSchema.post("findOneAndDelete", async(listing) => {
+    if(listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } });
+    }
+});
 
 
 const Listing = mongoose.model("Listing", listingSchema);
